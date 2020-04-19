7dd86abc1bda6da067f7e2ba431d64d2420f821a
solophone <solophone@aliyun.com>
2020-04-19 22:15:04 +0800

    添加排序方法

---
+++ src/basic/sort/bubbleSort.js
@@ -0,0 +1,16 @@
+const bubbleSort = function (arr) {
+  const { length } = arr;
+  for (let i = 0; i < length; i++) {
+    for (let j = 0; j < length - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        const temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+};
+
+const arr = [4, 52, 3, 1, 6, 7, 2, 4];
+console.log(bubbleSort(arr));
---
+++ src/basic/sort/selectionSort.js
@@ -0,0 +1,21 @@
+const selectionSort = function (arr) {
+  const { length } = arr;
+  let indexMin = 0; // 最小值的下标
+  for (let i = 0; i < length - 1; i++) {
+    indexMin = i;
+    for (let j = i; j < length; j++) {
+      if (arr[j] < arr[indexMin]) {
+        indexMin = j;
+      }
+    }
+    if (i !== indexMin) {
+      let temp = arr[i];
+      arr[i] = arr[indexMin];
+      arr[indexMin] = temp;
+    }
+  }
+  return arr;
+};
+
+const arr = [4, 52, 3, 1, 6, 7, 2, 4];
+console.log(selectionSort(arr));

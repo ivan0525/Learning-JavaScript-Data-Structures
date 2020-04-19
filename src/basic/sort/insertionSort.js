36c3d91d16bb6ecceafb1ec96d9ad52113c01cc7
solophone <solophone@aliyun.com>
2020-04-19 23:21:22 +0800

    归并排序实现

---
+++ src/basic/sort/insertionSort.js
@@ -0,0 +1,17 @@
+const insertionSort = function (arr) {
+  const { length } = arr;
+  let temp;
+  for (let i = 1; i < length; i++) {
+    let j = i;
+    temp = arr[i];
+    while (j > 0 && arr[j - 1] > temp) {
+      arr[j] = arr[j - 1];
+      j--;
+    }
+    arr[j] = temp;
+  }
+  return arr;
+};
+
+const arr = [3, 5, 1, 4, 2];
+console.log(insertionSort(arr));
---
+++ src/basic/sort/mergeSort.js
@@ -0,0 +1,23 @@
+const mergeSort = function (arr) {
+  if (arr.length > 1) {
+    const { length } = arr;
+    const middle = Math.floor(length / 2);
+    const left = mergeSort(arr.slice(0, middle));
+    const right = mergeSort(arr.slice(middle, length));
+    arr = merge(left, right);
+  }
+  return arr;
+};
+
+const merge = function (left, right) {
+  let i = 0;
+  let j = 0;
+  let result = [];
+  while (i < left.length && j < right.length) {
+    result.push(left[i] < right[j] ? left[i++] : right[j++]);
+  }
+  return result.concat(i < left.length ? left.slice(i) : right.slice(j));
+};
+
+const arr = [31, 23, 2, 4, 5, 6, 2, 6, 8, 89, 4, 32, 5, 1, 4, 2];
+console.log(mergeSort(arr));

5d34e0a83a2bca815c331d24feecd31b3e6c16e3
ivan0525 <zhuangyufeng189@gmail.com>
2021-08-18 11:11:05 +0800

    edit

--- src/basic/array/maxProfit.js
+++ src/basic/array/maxProfit.js
@@ -1,9 +1,9 @@
 // 买卖股票的最佳时机
 // 使用贪心算法
-function maxProfit(arr) {
+function maxProfit(prices) {
   let res = 0;
-  for (let i = 1; i < arr.length; i++) {
-    const temp = arr[i] - arr[i - 1];
+  for (let i = 1; i < prices.length; i++) {
+    const temp = prices[i] - prices[i - 1];
     if (temp > 0) {
       res+=temp;
     }
